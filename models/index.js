2b4229959e556aa17e8ef5e0ce623575f5903513
Jocephuss <rowlandjosiah14@gmail.com>
2024-08-03 18:11:58 -0600

    'models index.js complete'

--- models/index.js
+++ models/index.js
@@ -1,14 +1,37 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  // foreignKey is categoryId in Product table)
+  foreignKey: "categoryId",
+});
+
 // Categories have many Products
 
+Category.hasMany(Product, {
+  // foreignKey is productId in Category table)
+  foreignKey: "categoryId",
+});
+
 // Products belongToMany Tags (through ProductTag)
+
+Product.belongsToMany(Tag, {
+  // foreignKey is productId in Product table)
+  through: ProductTag, // through is the name of the join table
+  foreignKey: "productId", // otherKey is the name of the column in the join table referencing the other model (Tag)
+  otherKey: "tagId",
+});
+Tag.belongsToMany(Product, {
+  // foreignKey is tagId in Tag table)
+  through: ProductTag, // through is the name of the join table
+  foreignKey: "tagId", // otherKey is the name of the column in the join table referencing the other model (Product)
+  otherKey: "productId",
+});
 
 // Tags belongToMany Products (through ProductTag)
 
